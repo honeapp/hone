7139af84560bae1fbdbc3b958a44def00f3735d5
peterfiasco <peter.okachie@gmail.com>
2024-12-05 18:04:06 +0100

    feat: add cloudinary image support and mongodb adapter

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,12 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google"
+import FacebookProvider from "next-auth/providers/facebook"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "@/lib/mongodb"
 
 const handler = NextAuth({
+    adapter: MongoDBAdapter(clientPromise),
     providers: [
         CredentialsProvider({
             name: "Credentials",
@@ -30,6 +34,14 @@ const handler = NextAuth({
                     profileCompleted: user.profileCompleted
                 }
             }
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID!,
+            clientSecret: process.env.GOOGLE_SECRET!
+        }),
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_ID!,
+            clientSecret: process.env.FACEBOOK_SECRET!
         })
     ],
     callbacks: {
